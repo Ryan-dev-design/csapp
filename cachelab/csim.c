f945330c207c01a6fdcdc015fe72a11150a590ee
RyanLiu <ruoyanlau@gmail.com>
2024-05-25 09:12:22 +0000

    [add] finished cachelab

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -1,7 +1,172 @@
 #include "cachelab.h"
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-int main()
-{
-    printSummary(0, 0, 0);
+void printUsage(char* command) {
+    printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", command);
+    printf("Options:\n");
+    printf("  -h         Print this help message.\n");
+    printf("  -v         Optional verbose flag.\n");
+    printf("  -s <num> Number of set index bits.\n");
+    printf("  -E <num>   Number of lines per set.\n");
+    printf("  -b <num> Number of block bits.\n");
+    printf("  -t <file> Trace file.\n");
+    printf("\nExamples:\n");
+    printf("  linux>  %s -s 4 -E 1 -b 4 -t traces/yi.trace\n", command);
+    printf("  linux>  %s -v -s 8 -E 2 -b 4 -t traces/yi.trace\n", command);
+    exit(0);
+}
+
+struct line{
+    int valid;
+    int tag;
+    int lastUsedTime;
+};
+
+typedef struct line* set;
+
+set* createCache(int s, int E){
+    int S = 1 << s;
+    set* cache = (set*)malloc(sizeof(set) * S);
+    for(int i=0; i<S; ++i){
+        cache[i] = (set)malloc(sizeof(struct line) * E);
+        for(int j=0; j<E; ++j){
+            struct line* thisLine = &cache[i][j];
+            thisLine->valid = -1;
+            thisLine->tag = -1;
+            thisLine->lastUsedTime = -1;
+        } 
+    }
+    return cache;
+}
+
+void simCache(size_t address, bool isModify, int s, int E, int b, set* cache, bool verbose, int* hit, int* miss, int* eviction, int time){
+    int tag = address >> (b+s);
+    int setIndex = (address >> b) & ((1<<s)-1);
+    set thisSet = cache[setIndex];
+    int lruPos = 0, lruTime = thisSet[0].lastUsedTime;
+    for(int i=0; i<E; ++i){
+        if (thisSet[i].tag == tag){
+            ++(*hit);
+            if (isModify) ++(*hit);
+            thisSet[i].lastUsedTime = time;
+            if (verbose) printf("hit\n");
+            return;
+        }
+
+        // search for the least recent used line
+        if (thisSet[i].lastUsedTime < lruTime){
+            lruTime = thisSet[i].lastUsedTime;
+            lruPos = i;
+        }
+    }
+    ++(*miss);
+    if (lruTime!=-1){
+        ++(*eviction);
+        if (isModify){
+            ++(*hit);
+            if (verbose) printf("miss eviction hit\n");
+        }
+        else{
+            if(verbose) printf("miss eviction\n");
+        }
+    }
+    else{
+        if (isModify){
+            ++(*hit);
+            if (verbose) printf("miss hit\n");
+        }
+        else{
+            if (verbose) printf("miss\n");
+        }
+    }
+
+    thisSet[lruPos].lastUsedTime = time;
+    thisSet[lruPos].tag = tag;
+    return;
+
+}
+
+
+
+
+void run(int s, int E, int b, char* traceFile, bool verbose) {
+    char operation;
+    size_t address;
+    int size;
+    set* cache = createCache(s, E);
+
+    FILE* fp = fopen(traceFile, "r");
+
+    int hit = 0, miss = 0, eviction = 0, time = 0;
+    while (fscanf(fp, "%s %lx,%d\n", &operation, &address, &size) == 3)
+    {
+        if(operation == 'I') {
+            continue;
+        }
+        else if (operation == 'L' || operation == 'S') {
+            if (verbose) printf("%c %lx, %d ", operation, address, size);
+            simCache(address, false, s, E, b, cache, verbose, &hit, &miss, &eviction, time++);
+        }
+        else if (operation == 'M')
+        {
+            if (verbose) printf("%c %lx, %d ", operation, address, size);
+            simCache(address, true, s, E, b, cache, verbose, &hit, &miss, &eviction, time++);
+        }
+        
+    };
+    free(cache);
+    printSummary(hit, miss, eviction);
+
+}
+
+bool argValid(int s, int E, int b, char* traceFile) {
+    if (s <= 0 || E <= 0 || b <= 0 || traceFile == NULL) {
+        printf("Error: Invalid arguments.\n");
+        return false;
+    }
+    return true;
+}
+
+
+
+int main(int argc, char *argv[]) {
+    int opt;
+    int s_value, E_value, b_value;
+    bool verbose;
+    char *t_value;
+
+    while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+        switch (opt) {
+        case 's':
+            s_value = atoi(optarg);
+            break;
+        case 'v':
+            verbose = true;
+            break;
+        case 'E':
+            E_value = atoi(optarg);
+            break;
+        case 'b':
+            b_value = atoi(optarg);
+            break;
+        case 't':
+            t_value = optarg;
+            break;
+        default: /* '?' */
+            printUsage(argv[0]);
+            exit(0);
+        }
+    }
+    if(!argValid(s_value, E_value, b_value, t_value)) {
+        exit(1);
+    }
+    run(s_value, E_value, b_value, t_value, verbose);
+
+
+
     return 0;
 }
--- cachelab/trans.c
+++ cachelab/trans.c
@@ -22,6 +22,199 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int t1, t2, t3, t4, t5, t6, t7, t8 = 0;
+    if (M==32){
+        for (int i=0; i<N; i+=8){
+            for (int j=0; j<M; j+=8){
+                for(int k=0; k<8; k++){
+                    t1 = A[i+k][j];
+                    t2 = A[i+k][j+1];
+                    t3 = A[i+k][j+2];
+                    t4 = A[i+k][j+3];
+                    t5 = A[i+k][j+4];
+                    t6 = A[i+k][j+5];
+                    t7 = A[i+k][j+6];
+                    t8 = A[i+k][j+7];
+                    B[j][i+k] = t1;
+                    B[j+1][i+k] = t2;
+                    B[j+2][i+k] = t3;
+                    B[j+3][i+k] = t4;
+                    B[j+4][i+k] = t5;
+                    B[j+5][i+k] = t6;
+                    B[j+6][i+k] = t7;
+                    B[j+7][i+k] = t8;
+                }
+            }
+        }
+    }
+    else if(M==64){
+        for (int i=0; i<N; i+=8){
+            for (int j=0; j<M; j+=8){
+                for(int k=0; k<4; k++){
+                    t1 = A[i+k][j];
+                    t2 = A[i+k][j+1];
+                    t3 = A[i+k][j+2];
+                    t4 = A[i+k][j+3];
+                    t5 = A[i+k][j+4];
+                    t6 = A[i+k][j+5];
+                    t7 = A[i+k][j+6];
+                    t8 = A[i+k][j+7];
+
+                    B[j][i+k] = t1;
+                    B[j+1][i+k] = t2;
+                    B[j+2][i+k] = t3;
+                    B[j+3][i+k] = t4;
+                    B[j][i+k+4] = t5;
+                    B[j+1][i+k+4] = t6;
+                    B[j+2][i+k+4] = t7;
+                    B[j+3][i+k+4] = t8;
+                }
+                // right top of B to left bottom of B
+                // left bottom of A to right top of B
+                for (int k=0; k<4; k++){
+                    t5 = A[i+4][j+k];
+                    t6 = A[i+5][j+k];
+                    t7 = A[i+6][j+k];
+                    t8 = A[i+7][j+k];
+
+                    t1 = B[j+k][i+4];
+                    t2 = B[j+k][i+5];
+                    t3 = B[j+k][i+6];
+                    t4 = B[j+k][i+7];
+
+                    B[j+k][i+4] = t5;
+                    B[j+k][i+5] = t6;
+                    B[j+k][i+6] = t7;
+                    B[j+k][i+7] = t8;
+                    
+                    B[j+k+4][i] = t1;
+                    B[j+k+4][i+1] = t2;
+                    B[j+k+4][i+2] = t3;
+                    B[j+k+4][i+3] = t4;
+                    
+                }
+
+                for (int k=0; k<4; k+=2){
+                    t1 = A[i+4+k][j+4];
+                    t2 = A[i+4+k][j+5];
+                    t3 = A[i+4+k][j+6];
+                    t4 = A[i+4+k][j+7];
+                    t5 = A[i+5+k][j+4];
+                    t6 = A[i+5+k][j+5];
+                    t7 = A[i+5+k][j+6];
+                    t8 = A[i+5+k][j+7];
+
+                    B[j+4+k][i+4] = t1;
+                    B[j+4+k][i+5] = t2;
+                    B[j+4+k][i+6] = t3;
+                    B[j+4+k][i+7] = t4;
+                    B[j+5+k][i+4] = t5;
+                    B[j+5+k][i+5] = t6;
+                    B[j+5+k][i+6] = t7;
+                    B[j+5+k][i+7] = t8;
+                }
+
+                // transpose right bottom of B
+                for (int k=0; k<4; k++){
+                    for (int l=0; l<k; l++){
+                        t1 = B[j+4+k][i+4+l];
+                        t2 = B[j+4+l][i+4+k];
+                        B[j+4+k][i+4+l] = t2;
+                        B[j+4+l][i+4+k] = t1;
+                    }
+                }
+            }
+        }
+    }
+    else if (M==61)
+    {
+        int blocksize = 16;
+        for (int i=0; i<M; i+=blocksize){
+            for (int j=0; j<N; j+=blocksize){
+                for (int k=0; k<blocksize && k+j<N; k++){
+                    for (int l=0; l<blocksize && l+i<M; l++){
+                        if (k+j != l+i){
+                            B[l+i][k+j] = A[k+j][l+i];
+                        }
+                        else{
+                            t1 = A[k+j][l+i];
+                            t2 = k+j;
+                        }
+                    }
+                    if (i==j){
+                        B[t2][t2] = t1;
+                    }
+                }
+            }
+        }
+    }
+    
+    else{
+        for (int i=0; i<64; i+=8){
+            for (int j=0; j<56; j+=8){
+                for (int k=0; k<8; k++){
+                    t1 = A[i+k][j];
+                    t2 = A[i+k][j+1];
+                    t3 = A[i+k][j+2];
+                    t4 = A[i+k][j+3];
+                    t5 = A[i+k][j+4];
+                    t6 = A[i+k][j+5];
+                    t7 = A[i+k][j+6];
+                    t8 = A[i+k][j+7];
+
+                    B[j+k][i] = t1;
+                    B[j+k][i+1] = t2;
+                    B[j+k][i+2] = t3;
+                    B[j+k][i+3] = t4;
+                    B[j+k][i+4] = t5;
+                    B[j+k][i+5] = t6;
+                    B[j+k][i+6] = t7;
+                    B[j+k][i+7] = t8;
+                }
+                for (int k=0; k<8; k++){
+                    for (int l=0; l<k; l++){
+                        t1 = B[j+k][i+l];
+                        t2 = B[j+l][i+k];
+                        B[j+k][i+l] = t2;
+                        B[j+l][i+k] = t1;
+                    }
+                }
+            }
+        }
+        for (int i=0; i<64; i+=8){
+            for (int j=56; j<61; j++){
+                t1 = A[i][j];
+                t2 = A[i+1][j];
+                t3 = A[i+2][j];
+                t4 = A[i+3][j];
+                t5 = A[i+4][j];
+                t6 = A[i+5][j];
+                t7 = A[i+6][j];
+                t8 = A[i+7][j];
+
+                B[j][i] = t1;
+                B[j][i+1] = t2;
+                B[j][i+2] = t3;
+                B[j][i+3] = t4;
+                B[j][i+4] = t5;
+                B[j][i+5] = t6;
+                B[j][i+6] = t7;
+                B[j][i+7] = t8;
+
+            }
+        }
+
+        int i = 64;
+        for (int j=0; j<61; j++){
+            t1 = A[i][j];
+            t2 = A[i+1][j];
+            t3 = A[i+2][j];
+
+            B[j][i] = t1;
+            B[j][i+1] = t2;
+            B[j][i+2] = t3;
+        }
+    }    
 }
 
 /* 
